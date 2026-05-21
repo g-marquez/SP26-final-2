7f4310c1e3e2b79ab371275e6249c1ade7c247cb
Gabe Marquez <gabrielmarquez98@gmail.com>
2026-05-20 18:13:55 -0700

    update

--- SP26-final-2.cpp
+++ SP26-final-2.cpp
@@ -1,1 +1,17 @@
 // COMSC-210 | SP26-Final-2 | Gabriel Marquez
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Node {
+    string name;
+    string order;
+    Node *next;
+};
+
+int main() {
+
+
+    return 0;
+}
